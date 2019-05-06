88e4fbdbd77b02200791055ed2d941f4e09d6037
Mohit <mohitsinha810@gmail.com>
2019-05-06 13:28:42 +0545

    Complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Table from './Table';
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+	state = {characters: []};
+	removeCharacter = (index) => {
+		const {characters} = this.state;
+		this.setState({characters: characters.filter((character,i) => i != index )});
+	}
+	handleSubmit = (character) => {
+		this.setState({characters: [...this.state.characters,character]});
+	};
+	render(){
+		return (
+				<div className="container">
+					<h1>Dynamic Table Using React</h1>
+					<p>Fill in the form and click submit to insert values in the table.
+					Click delete to delete.</p>
+					<Form handleSubmit={this.handleSubmit} />
+					<Table 
+						characterData={this.state.characters} 
+						removeCharacter={this.removeCharacter} />
+				</div>
+			);
+	}
 }
 
 export default App;
